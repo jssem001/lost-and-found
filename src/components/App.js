9c9c7b81e4d8620e3d259879dbc3de508e0a7bad
skmutai6 <skmutai6@gmail.com>
2024-05-08 08:14:23 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React, {useState,useEffect} from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 import ToysTest from './ToysTest';
 
 function App() {
-  const [toys, setToys] = useState("")
+  const [toys, setToys] = useState([])
   
   useEffect(() =>{
     fetch("http://localhost:8002/toys")
@@ -14,22 +13,11 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <div className="App ">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/components/App.js</code> and save to reload.
-        </p>
-        <p>Team 9 welcome to your Phase 2 Project</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Dummy server info below</p>
+
+        <p>Welcome to your <span className='text-red-900'>lost</span> and <span className='text-green-900'>found</span> items page.</p>
+
         {toys.map(toy =>
             <ToysTest toys={toys} name={toy.name} image={toy.image} likes={toy.likes}/>
          )}
