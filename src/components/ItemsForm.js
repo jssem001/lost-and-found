27895a42d31f526e69d73215e97b1c0e1f3595ea
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-05-09 12:21:49 +0300

    final 2

--- src/components/ItemsForm.js
+++ src/components/ItemsForm.js
@@ -49,7 +49,7 @@ function ItemsForm({setItems}) {
       <input type="text" name="location" value={formdata.location} onChange={handleadd} class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
       placeholder=" " required />
       <label  class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-        Where last did you have it</label>
+        Where last did you have it/Where have you found it</label>
   </div>
  
 
